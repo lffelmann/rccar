7500cff29e6f9d4b0284720cd445e8baef1214a0
lffelmann <67123057+lffelmann@users.noreply.github.com>
2025-10-30 01:48:17 +0100

    Refactor Correction class to use FixedList for cmd_vel

--- src/correction.cpp
+++ src/correction.cpp
@@ -5,15 +5,39 @@
 #include "nav_msgs/msg/odometry.hpp"
 #include "sensor_msgs/msg/time_reference.hpp"
 #include <math.h>
+#include <deque>
+#include <iostream>
+
+
+template<typename T, size_t MAX_SIZE>
+class FixedList {
+private:
+    std::deque<T> dq;
+
+public:
+    FixedList() = default;
+
+    void push_front(const T& msg) {
+        if (dq.size() >= MAX_SIZE) {
+            dq.pop_back();
+        }
+        dq.push_front(msg);
+    }
+    
+    const T& peek_by_index(size_t index) const {
+        return dq.at(index);
+    }
+
+    bool is_full() const {
+        return dq.size() >= MAX_SIZE;
+    }
+};
 
 using std::placeholders::_1;
 
 class Correction : public rclcpp::Node {
   public:
     Correction() : Node("correction") {
-        this->declare_parameter<double>("alpha", 0.5);
-        alpha_ = this->get_parameter("alpha").as_double();
-
         auto qos_sensor = rclcpp::SensorDataQoS();
 
         sub_imu_ = this->create_subscription<sensor_msgs::msg::Imu>(
@@ -26,7 +50,7 @@ class Correction : public rclcpp::Node {
             "/cmd_vel", 1, std::bind(&Correction::cmd_vel_callback, this, _1));
 
         sub_imu_latency_ = this->create_subscription<sensor_msgs::msg::TimeReference>(
-            "/imu_latency", qos_sensor, std::bind(&Correction::imu_latency_callback, this, _1));
+            "/imu_latency", 1, std::bind(&Correction::imu_latency_callback, this, _1));
 
         pub_cmd_vel_imu_ = this->create_publisher<rccar_msgs::msg::TwistStampedTwistTimestampTimestamp>(
             "/cmd_vel_imu", 1);
@@ -36,8 +60,6 @@ class Correction : public rclcpp::Node {
     }
 
   private:
-    double alpha_;
-
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr sub_imu_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom_;
     rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr sub_cmd_vel_;
@@ -47,7 +69,7 @@ class Correction : public rclcpp::Node {
     sensor_msgs::msg::Imu::SharedPtr latest_imu_;
     nav_msgs::msg::Odometry::SharedPtr latest_odom_;
     sensor_msgs::msg::TimeReference::SharedPtr latest_imu_latency_;
-    geometry_msgs::msg::TwistStamped::SharedPtr last_cmd_vel_;
+    FixedList<geometry_msgs::msg::TwistStamped::SharedPtr, 100> cmd_vel_list_;
 
     void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg) { latest_imu_ = msg; }
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) { latest_odom_ = msg; }
@@ -78,52 +100,37 @@ class Correction : public rclcpp::Node {
             output_msg.stamp_1 = latest_odom_->header.stamp;
         }
 
-        if (!latest_imu_latency_) {
-          RCLCPP_INFO(this->get_logger(), "No latest_imu_latency");
-        }
-
-        if (!latest_imu_) {
-          RCLCPP_INFO(this->get_logger(), "No latest_imu");
-        }
-      
-        if (!last_cmd_vel_) {
-          RCLCPP_INFO(this->get_logger(), "No last_cmd_vel");
-        }
-      
-        if (!latest_imu_latency_ || !latest_imu_ || !last_cmd_vel_) {
+        if (!latest_imu_latency_ || !latest_imu_ || !cmd_vel_list_.is_full()) {
             output_msg.twist_1.angular.z = 999.9;
-            last_cmd_vel_ = msg;
+            cmd_vel_list_.push_front(msg);
             pub_cmd_vel_imu_->publish(output_msg);
             RCLCPP_INFO(this->get_logger(), "Published to /cmd_vel_imu without correction");
             return;
         }
 
+        sensor_msgs::msg::Imu::SharedPtr latest_imu = latest_imu_;
+
         double delta_t_imu = time_ref_to_float(latest_imu_latency_);
         double delta_t_cmd = 0.1;
-        double delta_t_imucmd = delta_t_imu - delta_t_cmd;
+         int cmds_used = static_cast<int>(delta_t_imu / delta_t_cmd);
+        double delta_t_calc = fmod(delta_t_imu, delta_t_cmd);
 
-        if (delta_t_imucmd < 0) {
-            delta_t_imucmd = 0.0;
-            delta_t_cmd = delta_t_imu;
+        double w = latest_imu->orientation.w;
+        double x = latest_imu->orientation.x;
+        double y = latest_imu->orientation.y;
+        double z = latest_imu->orientation.z;
+
+        double psi = atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z));
+        
+        for (int i = cmds_used; i>0; i--) {
+            double psi_vel_cmd = cmd_vel_list_.peek_by_index(i)->twist.angular.z;
+            psi += psi_vel_cmd * delta_t_calc;
+            delta_t_calc = delta_t_cmd;
         }
 
-        double w = latest_imu_->orientation.w;
-        double x = latest_imu_->orientation.x;
-        double y = latest_imu_->orientation.y;
-        double z = latest_imu_->orientation.z;
+        output_msg.twist_1.angular.z = psi;
 
-        double psi_vel_imu = latest_imu_->angular_velocity.z;
-        double psi_vel_cmd = last_cmd_vel_->twist.angular.z;
-
-        double psi_0 = atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z));
-        double psi_delta_t_imucmd = psi_0 + psi_vel_imu * delta_t_imucmd;
-        double psi_imu_delta_t_imu = psi_delta_t_imucmd + psi_vel_imu * delta_t_cmd;
-        double psi_cmd_delta_t_imu = psi_delta_t_imucmd + psi_vel_cmd * delta_t_cmd;
-        double psi_delta_t_imu = alpha * psi_imu_delta_t_imu + (1 - alpha) * psi_cmd_delta_t_imu;
-
-        output_msg.twist_1.angular.z = psi_delta_t_imu;
-
-        last_cmd_vel_ = msg;
+        cmd_vel_list_.push_front(msg);
         pub_cmd_vel_imu_->publish(output_msg);
         RCLCPP_INFO(this->get_logger(), "Published to /cmd_vel_imu");
     }
